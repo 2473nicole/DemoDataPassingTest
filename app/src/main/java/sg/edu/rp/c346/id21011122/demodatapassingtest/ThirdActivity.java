141ea0f79868afca56cdb6c28a594fc09aa42a78
GOH ZI QIN NICOLE <21011122@myrp.edu.sg>
2022-05-24 15:38:51 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21011122/demodatapassingtest/ThirdActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c346.id21011122.demodatapassingtest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    //Step 1
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        //Step 2
+        tvAnswer = findViewById(R.id.textView);
+        Intent intentReceived = getIntent();
+        char value = intentReceived.getCharExtra("value", 'a');
+        tvAnswer.setText("Character value received is: " + value);
+    }
+}
